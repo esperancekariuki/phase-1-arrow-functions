3f88dda64fa5646a56a2738374668b3516f23018
esperancekariuki <kariukiesperance@gmail.com>
2022-12-12 06:46:08 +0300

    RESUBMITTING ASSIGNMENT

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const divide = x=>200/100
-const square =param=>param*param
-const add = (t,y)=>t+y
+const divide =(x,y)=>2000/100
+const square =parameter=>parameter*parameter
+const add = (x,z)=>x+z
